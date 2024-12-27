4f5c9b8b42a2943c9abc825c39d8709194d6a6fe
“EngHusamSaad” <eng.husam.saed@gmail.com>
2024-12-27 21:46:38 +0200

    fix_some_bugs”

--- naqd_project/naqd_project/settings.py
+++ naqd_project/naqd_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +28,9 @@ SECRET_KEY = 'django-insecure-&z^f!8laiez1j0p6@emjy-06yqlp1+8+xasb(d!y4em@=41uvy
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Application definition
