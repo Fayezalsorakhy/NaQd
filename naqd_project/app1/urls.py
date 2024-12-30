99161a60b481b369956b117be4dddb403d4b22a7
EngHusamSaad <eng.husam.saed@gmail.com>
2024-12-30 12:53:53 +0200

    feature/debts_table

--- naqd_project/app1/urls.py
+++ naqd_project/app1/urls.py
@@ -6,6 +6,15 @@ urlpatterns = [
     path('main', views.main,name="main"),
     path('api/customers/', views.customers_list, name='customers-list'),
     path('api/debts/', views.debts_list, name='debts-list'),
+    
+    
+    
+    
+    path('select_customer/', views.select_customer, name='select_customer'),
+    path('customers_view', views.customers_view, name='customers_view'),
+
+    
+    
 
  
     
--- naqd_project/app1/views.py
+++ naqd_project/app1/views.py
@@ -45,3 +45,40 @@ def debts_list(request):
     return JsonResponse(data, safe=False)
 
 
+
+def select_customer(request):
+    pass
+    # if request.method == 'POST':
+    #     try:
+    #         print(request.body)
+
+    #         data = json.loads(request.body)
+    #         item_id = data.get('item_id')
+    #         customer_id = data.get('customer_id')
+            
+    #         customer_select=Customer.objects.get(id=customer_id)
+    #         item=Item.objects.get(id=item_id)
+            
+    #         item.customer=customer_select
+    #         item.isAvailble=False
+    #         item.sold_date=datetime.now()
+    #         item.save()
+    #         return JsonResponse({'success': True, 'message': f'{item.name_item} was sold Successfully by {customer_select.first_name} {customer_select.second_name} ! '})
+        
+    #     except json.JSONDecodeError as e:
+    #         return JsonResponse({
+    #     "error": "Invalid JSON format",
+    #     "message": str(e)
+    # }, status=400)
+    # else:
+    #         return JsonResponse({"error": "Invalid request method"}, status=405)
+
+
+
+def customers_view(request):
+    try:
+        customers = Customer.objects.all().values('id', 'first_name', 'second_name')
+        return JsonResponse(list(customers), safe=False)
+    except Exception as e:
+        print(f"Error fetching customers: {e}")
+        return JsonResponse({'error': 'Failed to retrieve customers'}, status=500)
