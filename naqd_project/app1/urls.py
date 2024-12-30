eb59e1c697f5fd88d79b97ab9ab10692568783f5
EngHusamSaad <eng.husam.saed@gmail.com>
2024-12-30 10:06:01 +0200

    feature/sidebar

--- naqd_project/app1/urls.py
+++ naqd_project/app1/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.login),   
-    path('register', views.register),  
+    path('register', views.register), 
+    path('main', views.main,name="main"),
+    path('api/customers/', views.customers_list, name='customers-list'),
+    path('api/debts/', views.debts_list, name='debts-list'),
+
+ 
+    
+    
 ]
--- naqd_project/app1/views.py
+++ naqd_project/app1/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from .models import *
+from django.http import JsonResponse
+
 
 
 
@@ -8,4 +11,37 @@ def login(request):
 
 def register(request):
     return render(request,"registration.html")
+
+
+def main(request):
+    return render(request,"main.html")
+
+
+def customers_list(request):
+    customers = Customer.objects.all()
+    data = [
+        {
+            'id': customer.id,
+            'first_name': customer.first_name,
+            'second_name': customer.second_name,
+            'email': customer.email,
+            'mobile': customer.mobile,
+        }
+        for customer in customers
+    ]
+    return JsonResponse(data, safe=False)
+
+def debts_list(request):
+    debts = Debt.objects.select_related('customer').all()
+    data = [
+        {
+            'id': debt.id,
+            'amount_debt': debt.amount_debt,
+            'status_debt': debt.status_debt,
+            'customer_name': f"{debt.customer.first_name} {debt.customer.second_name}",
+        }
+        for debt in debts
+    ]
+    return JsonResponse(data, safe=False)
+
 
